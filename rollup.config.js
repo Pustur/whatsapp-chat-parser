5851600609001ee56dfbe7d5a9d0410cbcde990a
Loris Bettazza <loris.bettazza@gmail.com>
2019-05-28 19:41:42 +0200

    :wrench: Added babel config, changed rollup config to generate the unminified and minified scripts.

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,35 @@
 import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
+import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
+
+const input = 'src/index.js';
+const name = 'whatsappChatParser';
+const format = 'umd';
+const exports = 'named';
+const babelOptions = {
+  exclude: 'node_modules/**',
+};
 
 export default [
   {
-    input: 'src/index.js',
+    input,
     output: {
-      name: 'whatsappChatParser',
+      name,
       file: pkg.browser,
-      format: 'umd',
-      exports: 'named',
+      format,
+      exports,
     },
-    plugins: [commonjs()],
+    plugins: [babel(babelOptions), commonjs()],
+  },
+  {
+    input,
+    output: {
+      name,
+      file: pkg.browser.replace(/\.js$/, '.min.js'),
+      format,
+      exports,
+    },
+    plugins: [babel(babelOptions), commonjs(), uglify()],
   },
 ];
