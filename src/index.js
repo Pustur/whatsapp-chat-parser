3cd95c4a6cba95f8e140ac09c790fcc79b1c06c3
Loris Bettazza <loris.bettazza@gmail.com>
2019-05-29 17:26:10 +0200

    :fire: Remove processPromiseResult function
    It prevented some duplication when the method parseFile was still present, but now it has no purpose

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 const { makeArrayOfMessages, parseMessages } = require('./parser.js');
 
 /**
- * Given a promise that will resolve in a string it will process and parse it
- */
-function processPromiseResult(promise, options) {
-  return promise
-    .then(data => data.split('\n'))
-    .then(makeArrayOfMessages)
-    .then(messages => parseMessages(messages, options));
-}
-
-/**
  * Given a string it will parse its content
  * Returns a promise that will contain the parsed messages
  */
 function parseString(string, options) {
-  return processPromiseResult(Promise.resolve(string), options);
+  return Promise.resolve(string)
+    .then(data => data.split('\n'))
+    .then(makeArrayOfMessages)
+    .then(messages => parseMessages(messages, options));
 }
 
 module.exports = { parseString };
